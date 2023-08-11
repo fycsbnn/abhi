0ac592ea7d426ee7b5fd4caf3075e17987275392
fycsbnn <141981980+fycsbnn@users.noreply.github.com>
2023-08-11 09:45:11 +0530

    Create fycs.c

---
+++ fycs.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{int main i,j,row;
+printf("enter the value");
+scanf("%d",row);
+for(i=1;i<=row;i++)
+  { for(j=1;j<=i;j++)
+    {  printf("%d");}
+printf(\n);
+  }
+  }
